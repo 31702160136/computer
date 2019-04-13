3792df18ef04b9817de5490b5c809293755b3695
31702160137 <1041680485@qq.com>
2019-04-13 22:59:34 +0800

    前台首页基本框架

---
+++ front_desk/index.php
@@ -0,0 +1,303 @@
+<!DOCTYPE html>
+<html >
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>茂名职业技术学院 - 计算机工程系</title>
+<meta name="Keywords" content="茂名职业技术学院,计算机工程系" />
+<meta name="Description" content="计算机工程系" />
+  <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no">
+
+	<!--bootstrap-->
+	<link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.css">
+		
+	<script src="js/jquery.v1.9.1min.js"></script>
+	<script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script> 
+	
+	<link rel="stylesheet" type="text/css" href="css/swiper.min.css"/>
+	<link rel="stylesheet" type="text/css" href="css/certify.css"/>
+	<script src="js/swiper.min.js"></script>
+	
+	<!--jquery carousel-->
+	<link rel="stylesheet" href="OwlCarousel2-2.3.4/dist/assets/owl.carousel.css">
+	<link rel="stylesheet" href="OwlCarousel2-2.3.4/docs/assets/owlcarousel/assets/owl.theme.default.min.css">
+	<script src="OwlCarousel2-2.3.4/dist/owl.carousel.js"></script>
+	
+</head>
+<style type="text/css">
+	body{
+		background: rgb(17,168,255);
+	}
+	#header-bg-blue{
+		background: rgb(17,168,255);
+	}
+	#header-bg-white{
+		background: rgb(17,168,255);
+	}
+	.header-logo{
+		padding-bottom: 15px;
+		padding-top: 15px;
+	}
+	.header-title{
+		font-family: "华文行楷";
+		font-size: 48px;
+		color: #FFFFFF;
+	}
+	.news{
+		color: #FFFFFF;
+	}
+	.news h3{
+		background: rgba(0,0,0,.1);
+	}
+	.footer{
+		color: #FFFFFF;
+		background: rgb(127,36,161);
+	}
+	.footer img{
+		margin-top:15px;
+		margin-left: 15px;
+		width: 100%;
+	}
+	.footer p{
+		padding-left: 10px;
+	}
+	.mt20{
+		margin-top: 20px;
+	}
+	.mb20{
+		margin-bottom: 20px;
+	}
+</style>
+<body> 
+<!--头部 start-->
+<header>
+<nav id="header-bg-blue" role="navigation">
+    <div class="container"> 
+    	<div class="header-logo">
+		    <div class="container-fluid">
+		      <div class="row">
+		        <div class="col-md-6">
+		        	<img class="img-responsive" src="images/logoarea.png"/></div>
+		        <div class="col-md-6 visible-md-inline visible-lg-inline"> 
+		         	<div class="header-title">计算机工程系</div>
+		        </div>
+		      </div>
+		    </div>
+		  </div>
+    </div>
+</nav>
+<nav class="navbar navbar-default">
+	<div class="container">
+		<div class="row">
+		<div id="header-bg-white" class="navbar-header col-lg-12">
+				<a class="navbar-brand visible-xs-inline" href="#"><span>导航</span></a>
+        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                data-target="#example-navbar-collapse">
+            <span class="sr-only">切换导航</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse col-lg-12" id="example-navbar-collapse">
+        <ul class="nav navbar-nav navbar-right" >
+             <li class="active"><a href="#">系部首页</a></li>
+             <li class="active"><a href="#">系部新闻</a></li>
+             <li class="active"><a href="#">系部概况</a></li>
+             <li class="active"><a href="#">科学教研</a></li>
+             <li class="active"><a href="#">党建工作</a></li>
+             <li class="active"><a href="#">学生工作</a></li>
+             <li class="active"><a href="#">招生就业</a></li>
+             <li class="active"><a href="#">教师风采</a></li>
+             <li class="active"><a href="#">制度建设</a></li>
+             <li class="active"><a href="#">技能竞赛</a></li>
+             <li class="active"><a href="#">学院首页</a></li>
+        </ul>
+    </div>
+		</div>
+	</div>
+</nav>
+</header>
+<!--头部 end-->
+
+<!--轮播图 start-->
+<div id="owl-demo" class="owl-carousel owl-theme">
+   <a class="item" href="#"><img src="images/1.jpg" alt=""></a> 
+   <a class="item" href="#"><img src="images/2.jpg" alt=""></a> 
+   <a class="item" href="#"><img src="images/3.jpg" alt=""></a>
+   <a class="item" href="#"><img src="images/4.jpg" alt=""></a>
+</div>
+<!--轮播图 end-->
+<hr />
+<!--图片新闻 start-->
+<div id="certify">
+<div class="swiper-container">
+<div class="swiper-wrapper">
+<div class="swiper-slide"><img style="height: 370px;width: 100%;" src="images/p1.png" /><p>Picture information 1</p></div>
+<div class="swiper-slide"><img style="height: 370px;width: 100%;" src="images/p2.png" /><p>Picture information 2</p></div>
+<div class="swiper-slide"><img style="height: 370px;width: 100%;" src="images/p3.png" /><p>Picture information 3</p></div>
+<div class="swiper-slide"><img style="height: 370px;width: 100%;" src="images/p4.png" /><p>Picture information 4</p></div>
+<div class="swiper-slide"><img style="height: 370px;width: 100%;" src="images/p5.png" /><p>Picture information 5</p></div>
+</div>
+</div>
+<div class="swiper-pagination"></div>
+<div class="swiper-button-prev"></div>
+<div class="swiper-button-next"></div>
+</div>
+<!--图片新闻 end-->
+
+<!--文字新闻 strat-->
+<section class="container-fluid">	
+	<div class="row ">
+		<div class="col-md-3 news news-left" >
+			<h3>这是新闻区域</h3>
+			<p>这是新闻</p>
+			<p>这是新闻</p>
+			<p>这是新闻</p>
+		</div>
+		<div class="col-md-3 news col-md-offset-1" >
+			<h3>这是新闻区域</h3>
+			<p>这是新闻</p>
+			<p>这是新闻</p>
+			<p>这是新闻</p>
+		</div>
+		<div class="col-md-3 news col-md-offset-1">
+			<h3>这是新闻区域</h3>
+			<p>这是新闻</p>
+			<p>这是新闻</p>
+			<p>这是新闻</p>
+		</div>
+	</div>
+</section>
+<!--文字新闻 end-->
+
+<!--通知 科研 start-->
+<section class="container-fluid mt20 mb20">
+	<div class="row">
+		<div class="col-sm-12 col-md-3 col-lg-offset-1 col-md-offset-1 mb20" style=" height: 200px; background: #999;">
+			<h3>通知公告</h3>
+			<p>这是通知</p>
+			<p>这是通知</p>
+			<p>这是通知</p>
+		</div>
+	  <div class="col-sm-6 col-md-4">
+	    <div class="thumbnail">
+	      <img src="images/ky2.jpg" alt="">
+	      <div class="caption">
+	        <h3>Thumbnail label</h3>
+	        <p>example</p>
+	        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-6 col-md-4 deviation">
+	    <div class="thumbnail">
+	      <img src="images/ky1.jpg" alt="">
+	      <div class="caption">
+	        <h3>Thumbnail label</h3>
+	        <p>example</p>
+	        <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+</section>
+
+<footer style="clear: left;">
+	<div class="container-fluid footer">
+	  	<div>
+	  		<div style="float: left;width: 300px;"><img src="images/logoarea.png"/></div>
+	  		<div style="padding-left: 30px;padding-top:10px;float: left;line-height: 40px;">
+	  			<span style="clear: both;">电话:0668-2920526&nbsp;&nbsp;电子邮件：mzjsj@126.com</span><br />
+	  			<span>地址：茂名师电白区沙院镇海城五路1号</span>
+	  		</div>
+	  	</div>
+	  <br style="clear: left;" />
+	  <hr />
+	  <p style="text-align: center;">Cpoyright 2004-2017茂名职业技术学院 / 计算机工程系 All Rights Reserved.</p>	  		
+	</div>
+</footer>
+
+<script>
+	$(function(){
+		// 初始化轮播
+		$(".start-slide").click(function(){
+			$("#myCarousel").carousel('cycle');
+		});
+		// 停止轮播
+		$(".pause-slide").click(function(){
+			$("#myCarousel").carousel('pause');
+		});
+		// 循环轮播到上一个项目
+		$(".prev-slide").click(function(){
+			$("#myCarousel").carousel('prev');
+		});
+		// 循环轮播到下一个项目
+		$(".next-slide").click(function(){
+			$("#myCarousel").carousel('next');
+		});
+		// 循环轮播到某个特定的帧 
+		$(".slide-one").click(function(){
+			$("#myCarousel").carousel(0);
+		});
+		$(".slide-two").click(function(){
+			$("#myCarousel").carousel(1);
+		});
+		$(".slide-three").click(function(){
+			$("#myCarousel").carousel(2);
+		});
+		
+		$("#owl-demo").owlCarousel({
+			items:1,
+			autoPlay:true
+		});
+	var certifySwiper = new Swiper('#certify .swiper-container', {
+	watchSlidesProgress: true,
+	slidesPerView: 'auto',
+	centeredSlides: true,
+	loop: true,
+	loopedSlides: 3,
+	autoplay: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		//clickable :true,
+	},
+	on: {
+		progress: function(progress) {
+			for (i = 0; i < this.slides.length; i++) {
+				var slide = this.slides.eq(i);
+				var slideProgress = this.slides[i].progress;
+				modify = 1;
+				if (Math.abs(slideProgress) > 1) {
+					modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
+				}
+				translate = slideProgress * modify * 260 + 'px';
+				scale = 1 - Math.abs(slideProgress) / 5;
+				zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
+				slide.transform('translateX(' + translate + ') scale(' + scale + ')');
+				slide.css('zIndex', zIndex);
+				slide.css('opacity', 1);
+				if (Math.abs(slideProgress) > 3) {
+					slide.css('opacity', 0);
+				}
+			}
+		},
+		setTransition: function(transition) {
+			for (var i = 0; i < this.slides.length; i++) {
+				var slide = this.slides.eq(i)
+				slide.transition(transition);
+			}
+
+		}
+	}
+
+});
+
+	});
+</script>
+</body>
+</html>
